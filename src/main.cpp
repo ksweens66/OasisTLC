5cac20d1d6d6a7bdd78e8eeef6b3add6bbd95b34
ksweens66 <ksweens66@gmail.com>
2020-05-21 13:22:07 -0400

    idk why it wants met o commit again

--- src/main.cpp
+++ src/main.cpp
@@ -8,36 +8,14 @@ Sends important data to Beaglebone
 
 #include <Arduino.h>
 #include <kinetis.h>
+#include<Constants.h>
 
-#define LEDPIN 11
+double e = 0;                                         // Error from the target temperature, 0 at startup so not null
+float x = 0;                                          // integrater term, 0 at startup so not null
+double u = 0;                                         // Duty cycle, 0 at startup so not null
  
 IntervalTimer wdTimer;                                // Initialize Interval Timer
-
-const int       frequency = 93750;
-const int       resistor = 10000;                     // ohms of the voltage divider resistor
-const float     roomTemp = 298.15;                    // roomTemp in Kelvin
-const float     roomResistance = 20000;               // resistance at roomTemp
-const int       beta = 4300;                          // beta value of thermistor
-const double    a = 0.001159;                         // taken using dataset and matlab
-const double    b = 2.128e-04;                        // taken using dataset and matlab
-const double    c = 8.974e-8;                         // taken using dataset and matlab
-const int       baudRate = 115200;                    // baud rate
-const int       analogBitCount = 14;                  // analog bit count
-const int       analogMax = (1 << analogBitCount) - 1;// maximum analog value
-const int       avgThermLoop = 5;                     // number of loops to calculate the average thermistor value
-const int       PWM_pin = 4;                          // A changeable pin value, 4 is a placeholder for now
-int percent =   analogMax / 2;                        // initializes the percent to 50% of the maximum analog value
-
-const int TS = 100;                                   // Time sample
-const float KP = .25;                                 // Proportional gain
-const float KI = .05;                                 // Integral gain
-float x = 0;                                          // Initial value of integrater
-const float TF_SP = 300;                              // Target temperature
-double e = 0;                                         // Error from the target temperature
-double u = 0;                                         // Duty cycle
-
-             
-
+            
 void WatchDogReset();
 void setup() {
     Serial.begin(baudRate);                       // change baud rate accordingly
@@ -68,13 +46,10 @@ float calcTemp() {
 
     for (int i = 0; i < avgThermLoop; i++) {
         int analog1 = analogRead(A1);             // change pin accordingly, measures analog value of thermistor1
-        
         Serial.print("Analog Value:          ");
         Serial.println(analog1);
-        
         float thermistor1 = (analog1 * resistor) / (analogMax - analog1);                       // calculates resistance of thermistor1
-        float temperature1 = 1 / (a + b * log(thermistor1) + c * pow(log(thermistor1), 3));
-        
+        float temperature1 = 1 / (a + b * log(thermistor1) + c * pow(log(thermistor1), 3));       
         Serial.print("Resistance Calculated: ");
         Serial.println(thermistor1);
         
@@ -97,8 +72,8 @@ void WatchDogReset() {
 }
 
 void loop() {
-    digitalWrite(LEDPIN, HIGH);
-    delay (1000);
+    digitalWrite(LEDPIN, HIGH);                     // turns LED on
+    delay (1000);                                   // used to slow LED blink 
     
     /* Realize PI Controller */
     e = TF_SP - calcTemp(); // error is the SET_POINT - ACTUAL (TF_PV)
