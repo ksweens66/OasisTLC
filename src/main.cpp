f60f2c23c4ed0d65f5166e2ee94d88abd333768c
ksweens66 <ksweens66@gmail.com>
2020-05-28 15:20:44 -0400

    removed appended semicolon from thermistor readings as it is not needed with the prepended "|"

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ void loop() {
                 BBB_comms.print(mini_temperature + ',');                // last value gets printed with a ; to mark end
                 duty_cycle_str = String(duty_cycle, DEC);
                 mini_duty_cycle = duty_cycle_str.substring(0,6);
-                BBB_comms.println(mini_duty_cycle + ';');   // print the duty cycle to make Normen happy
+                BBB_comms.println(mini_duty_cycle);   // print the duty cycle to make Normen happy
                 }               
         }
         // for PID debugging. Prints the duty cycle.
