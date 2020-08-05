5aeb08cfbefdbd07b5151f151eb1b572d8759591
amk6451 <44247590+amk6451@users.noreply.github.com>
2020-08-04 20:10:04 -0400

    Update thermistorCalcs.cpp

--- src/thermistorCalcs.cpp
+++ src/thermistorCalcs.cpp
@@ -12,8 +12,17 @@ double calcTemp(double analog_value) {
 }
 
 double selected_PID_input(double calculated_temperatures[thermistor_count]){
-    double sorted_temperatures[thermistor_count];
-    memcpy(sorted_temperatures,calculated_temperatures,sizeof(double)* thermistor_count);
-    std::sort(sorted_temperatures,sorted_temperatures + thermistor_count);
-    return sorted_temperatures[middle_index];
+//     double sorted_temperatures[thermistor_count];
+//     memcpy(sorted_temperatures,calculated_temperatures,sizeof(double)* thermistor_count);
+//     std::sort(sorted_temperatures,sorted_temperatures + thermistor_count);
+//     return sorted_temperatures[middle_index];
+        
+        //Schiano's Standard Deviation Method
+        //1. First find the mean of the sensors
+        //2. Find the error between each sensor and the mean
+        //
+        //3. use the definition of standard deviation
+        //std = (
+        //4.
+        //if abs(k) > 3*std; reject error of (k)
 }
