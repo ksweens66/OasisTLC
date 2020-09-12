79f06280f93c47a06635f058e3b017b3a8af4eeb
amk6451 <44247590+amk6451@users.noreply.github.com>
2020-09-12 17:54:05 -0400

    Update thermistorCalcs.cpp

--- src/thermistorCalcs.cpp
+++ src/thermistorCalcs.cpp
@@ -11,9 +11,39 @@ double calcTemp(double analog_value) {
         return thermistor_temperature;       
 }
 
-double selected_PID_input(double calculated_temperatures[thermistor_count]){
-    double sorted_temperatures[thermistor_count];
-    memcpy(sorted_temperatures,calculated_temperatures,sizeof(double)* thermistor_count);
-    std::sort(sorted_temperatures,sorted_temperatures + thermistor_count);
-    return sorted_temperatures[middle_index];
-}
+double selected_PID_input(double temperature_readings[]){
+  //takes in a static array of 9 temperature values and returns a double setpoint
+ // records data to 2 standard deviation
+{
+  double average = 0;
+  double sum = 0;
+  double sdh = 0;
+  double twosd = 0;
+  double abserrori = 0;
+  double newsetpoint = 0;
+  double stackcounter = 0;
+  double setpointsum = 0;
+
+  for (int i = 0; i < 9; i++) //gets all 9 thermistor values
+  {
+    sum += temperature_readings[i];
+  }
+  average = sum / 9; //calculates the average
+
+  for (int i = 0; i < 9; i++)
+  {
+    sdh += pow((temperature_readings[i] - average), 2);
+  }
+  twosd = 2 * sqrt(sdh / 9);
+
+  for (int i = 0; i < 9; i++)
+  {
+    abserrori = abs(temperature_readings[i] - average);
+    if (abserrori < twosd) //reject sensor data for values greater than 2 sd
+    {
+      setpointsum = setpointsum + temperature_readings[i]; //add values within 2sd and takes new average
+      stackcounter = stackcounter + 1;
+    }
+  }
+  newsetpoint = setpointsum/stackcounter;
+  return newsetpoint;
